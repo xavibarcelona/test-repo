1ac1f03cd761a01317eafd96618abd5eca288535
xavi <jmt.spain@gmail.com>
2014-11-12 11:52:06 +0100

    testingz

---
+++ data analysis fundamentals course.R
@@ -0,0 +1,15 @@
+#simplecalculations
+3+4
+3*4
+3-4
+sqrt(25)
+x <- 5
+y <- x^2
+myvariable <- c(72, 28, -9, 12, 11)
+myvariable*3
+sqrt(myvariable)
+myvariable2 <- myvariable*3
+mean(myvariable)
+length(myvariable)
+sum(myvariable)
+z <- c(2,4,6,8,10)
